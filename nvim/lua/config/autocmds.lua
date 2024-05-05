399a975a0e3ba630a3083bc957b8bde8786e9248
Triet Le <triet.lq@holistics.io>
2024-05-05 21:53:41 +0700

    feat: auto toggle git blame when open vim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("toggle_blame_line"),
+  callback = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.toggle_current_line_blame()
+  end,
+})
