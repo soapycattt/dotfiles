6c8933bbdbf2b4ed8437b668ed2bba7db3c0d12f
quynhethereal <triet.lq@holistics.io>
2024-09-10 14:12:18 +0700

    chore: disabled obsidian

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "nvim-neo-tree/dashboard.nvim", enabled = false },
   { "mini.comment", enabled = false },
+  { "epwalsh/obsidian.nvim", enabled = false },
 }
